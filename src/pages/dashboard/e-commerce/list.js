b860ab88dbac248a524ce5ffdee9b4ad3cc42e13
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2023-07-26 16:34:56 -0500

    Settings in List Cache

--- src/pages/dashboard/e-commerce/list.js
+++ src/pages/dashboard/e-commerce/list.js
@@ -97,7 +97,9 @@ export default function EcommerceProductListPage() {
 
   const dispatch = useDispatch();
 
-  const { products, isLoading } = useSelector((state) => state.product);
+  const [isLoading, setIsLoading] = useState(false);
+  //const { products, isLoading } = useSelector((state) => state.product);
+  const [ products, setProducts ] = useState([]);
 
   const [tableData, setTableData] = useState([]);
 
@@ -107,9 +109,41 @@ export default function EcommerceProductListPage() {
 
   const [openConfirm, setOpenConfirm] = useState(false);
 
+  // useEffect(() => {
+  //   dispatch(getProducts());
+  // }, [dispatch]);
+
   useEffect(() => {
-    dispatch(getProducts());
-  }, [dispatch]);
+    const fetchData = async () => {
+      try {
+        const cache = await caches.open('cache-crm');
+        const response = await cache.match('https://crm.lidenar.com/hanadb/api/products');
+
+        if (response) {
+          // Si hay una respuesta en la caché, se obtiene su contenido
+          const cachedData = await response.json();
+          setProducts(cachedData.products);
+          console.log(cachedData);
+        }
+
+        // Independientemente de si hay una respuesta en la caché o no, se realiza la solicitud de red
+        const networkResponse = await fetch('https://crm.lidenar.com/hanadb/api/products');
+        const data = await networkResponse.json();
+
+        // Se almacena la respuesta de red en la caché
+        await cache.put('https://crm.lidenar.com/hanadb/api/products', new Response(JSON.stringify(data)));
+
+        // Si había una respuesta en la caché, los productos ya se establecieron en el estado
+        // Si no había respuesta en la caché, ahora se establecen los productos con los datos de la respuesta de red
+        setProducts(data.products);
+        console.log(data);
+      } catch (error) {
+        console.error('Error al obtener los datos:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   useEffect(() => {
     if (products.length) {
