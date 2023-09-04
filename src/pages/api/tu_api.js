6e6162e2ca4e182025ab7e1da2cc4938bd4d3ab6
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2023-09-04 15:18:33 -0500

    Demo Api

---
+++ src/pages/api/tu_api.js
@@ -0,0 +1,4 @@
+export default function handler(req, res) {
+    // Aquí puedes realizar la lógica para responder a la solicitud de la API
+    res.status(200).json({ message: 'Ejemplo de API en Next.js' });
+}
