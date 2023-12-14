18a73338b1a3404fe3f837d98f1205bae6073f32
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2023-12-14 11:51:47 -0500

    Página para ingresar IMEIs V2

--- src/pages/dashboard/invoice/imeis.js
+++ src/pages/dashboard/invoice/imeis.js
@@ -22,6 +22,9 @@ InvoiceCreatePage.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout
 export default function InvoiceCreatePage() {
     const {themeStretch} = useSettingsContext();
 
+    const [textArrayCount, setTextArrayCount] = useState(0);
+    const [uniqueTextArrayCount, setUniqueTextArrayCount] = useState(0);
+
     const FileCopySvgIcon = (props) => (
         <SvgIcon {...props}>
             <path fill-rule="evenodd" clip-rule="evenodd" d="M12 4C10.8954 4 10 4.89543 10 6H14C14 4.89543 13.1046 4 12 4ZM8.53513 4C9.22675 2.8044 10.5194 2 12 2C13.4806 2 14.7733 2.8044 15.4649 4H17C18.6569 4 20 5.34315 20 7V19C20 20.6569 18.6569 22 17 22H7C5.34315 22 4 20.6569 4 19V7C4 5.34315 5.34315 4 7 4H8.53513ZM8 6H7C6.44772 6 6 6.44772 6 7V19C6 19.5523 6.44772 20 7 20H17C17.5523 20 18 19.5523 18 19V7C18 6.44772 17.5523 6 17 6H16C16 7.10457 15.1046 8 14 8H10C8.89543 8 8 7.10457 8 6Z" fill="currentColor"></path>
@@ -31,19 +34,36 @@ export default function InvoiceCreatePage() {
     const [text, setText] = useState('');
     const [buttonDisabled, setButtonDisabled] = useState(false);
 
+    const [clickCount, setClickCount] = useState(0);
+
+
     const handleTextChange = (event) => {
         const inputText = event.target.value;
         const textArray = inputText.split('\n').map((item) => item.trim());
+        setTextArrayCount(textArray.length);
         setText(textArray.join('\n'));
     };
 
     const handlePrintClick = () => {
         setButtonDisabled(true);
         const textArray = text.split('\n').map((item) => item.trim()).filter(Boolean); // Eliminar líneas vacías
-        const sortedText = textArray.sort().join(',\n');
+
+        setTextArrayCount(textArray.length);
+
+        const uniqueTextArray = [...new Set(textArray)]; // Eliminar duplicados usando un Set
+        setUniqueTextArrayCount(uniqueTextArray.length);
+
+
+        const sortedText = uniqueTextArray.sort().join(',\n');
         setText(sortedText);
         console.log(sortedText);
         // Puedes agregar aquí una lógica adicional después de imprimir en la consola, si es necesario
+
+        setClickCount((prevCount) => prevCount + 1);
+
+        if (clickCount >= 1) {
+            setButtonDisabled(true);
+        }
 
         // Copiar al portapapeles
         navigator.clipboard.writeText(sortedText).then(() => {
@@ -54,6 +74,12 @@ export default function InvoiceCreatePage() {
         });
     };
 
+    const handleClearClick = () => {
+        setText('');
+        setTextArrayCount(0);
+        setUniqueTextArrayCount(0);
+        setButtonDisabled(false); // Asegúrate de habilitar el botón después de limpiar.
+    };
 
 
     return (
@@ -75,7 +101,7 @@ export default function InvoiceCreatePage() {
                             href: PATH_DASHBOARD.invoice.list,
                         },
                         {
-                            name: 'New invoice',
+                            name: 'SAP',
                         },
                     ]}
                 />
@@ -83,9 +109,19 @@ export default function InvoiceCreatePage() {
                 <h2>IMEIs</h2>
 
                 <div>
-                    <IconButton variant="outlined" onClick={handlePrintClick} disabled={buttonDisabled}>
-                        <FileCopySvgIcon />
-                    </IconButton>
+
+                    <div style={{display: 'flex', alignItems: 'center'}}>
+                        <p style={{marginRight: '10px'}}>Líneas ingresadas: {textArrayCount}</p>
+                        <p style={{marginRight: '10px', color: "red"}}>TOTAL ITEMs COPIADOS: {uniqueTextArrayCount}</p>
+
+                        <IconButton variant="outlined" onClick={handlePrintClick} disabled={buttonDisabled}>
+                            <FileCopySvgIcon/>
+                        </IconButton>
+
+                        <Button variant="outlined" onClick={handleClearClick} style={{marginLeft: '10px'}}>
+                            Limpiar
+                        </Button>
+                    </div>
 
                     <TextField
                         rows={100}
