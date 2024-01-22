26de9cd46bc6d458087adcb7e54d176eed778e86
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2024-01-22 15:17:33 -0500

    Ajuste Login

--- src/sections/auth/Login.js
+++ src/sections/auth/Login.js
@@ -1,19 +1,7 @@
 // next
 import NextLink from 'next/link';
 // @mui
-import {
-    Alert,
-    Tooltip,
-    Stack,
-    Typography,
-    Link,
-    Box,
-    AlertTitle,
-    Button,
-    CardHeader,
-    CardContent,
-    Card
-} from '@mui/material';
+import {Alert, Tooltip, Stack, Typography, Link, Box, AlertTitle, Button} from '@mui/material';
 // auth
 import { useAuthContext } from '../../auth/useAuthContext';
 // layouts
@@ -26,16 +14,11 @@ import AuthWithSocial from './AuthWithSocial';
 import React, {useEffect, useState} from "react";
 import Iconify from "../../components/iconify";
 import Label from "../../components/label";
-import MapChangeTheme from "../_examples/extra/map/change-theme";
-import {styled} from "@mui/material/styles";
-import {MAP_API} from "../../config-global";
-import {MapControl} from "../../components/map";
-import Map from "react-map-gl";
 
 // ----------------------------------------------------------------------
 
 export default function Login() {
-  const { method } = useAuthContext();
+    const { method } = useAuthContext();
 
     const [latitude, setLatitude] = useState(null);
     const [longitude, setLongitude] = useState(null);
@@ -66,95 +49,64 @@ export default function Login() {
         setError('Para activar la geolocalización, por favor ve a la configuración del navegador y habilita los permisos de ubicación.');
     };
 
-  return (
-      <LoginLayout>
-      <div>
-          {error ? (
+    return (
+        <LoginLayout>
+            <div>
+                {error ? (
 
-                  <Stack spacing={2}>
+                    <Stack spacing={2}>
 
-                      <Alert key="error" severity="error">
-                          <AlertTitle sx={{textTransform: 'capitalize'}}> Error de Geolocalización </AlertTitle>
-                          {error}
-                      </Alert>
-                      <Button color="info" size="small" variant="soft" onClick={handleOpenSettings}>
-                          Instrucciones
-                      </Button>
-                  </Stack>
-          ) : (
-              <div>
-                  <Stack spacing={2} sx={{mb: 5, position: 'relative'}}>
-                      <Typography variant="h4">Iniciar sesión en Lidenar</Typography>
+                        <Alert key="error" severity="error">
+                            <AlertTitle sx={{textTransform: 'capitalize'}}> Error de Geolocalización </AlertTitle>
+                            {error}
+                        </Alert>
+                        <Button color="info" size="small" variant="soft" onClick={handleOpenSettings}>
+                            Instrucciones
+                        </Button>
+                    </Stack>
+                ) : (
+                    <div>
+                        <Stack spacing={2} sx={{mb: 5, position: 'relative'}}>
+                            <Typography variant="h4">Iniciar sesión en Lidenar</Typography>
 
-                          <Stack direction="row" spacing={0.5}>
-                              <Typography variant="body2">¿Nuevo usuario?</Typography>
+                            <Stack direction="row" spacing={0.5}>
+                                <Typography variant="body2">¿Nuevo usuario?</Typography>
 
-                              <Link component={NextLink} href={PATH_AUTH.register} variant="subtitle2">
-                                  Crea una cuenta
-                              </Link>
-                          </Stack>
+                                <Link component={NextLink} href={PATH_AUTH.register} variant="subtitle2">
+                                    Crea una cuenta
+                                </Link>
+                            </Stack>
 
-                          <Tooltip title={method} placement="left">
-                              <Box
-                                  component="img"
-                                  alt={method}
-                                  src={`/assets/icons/auth/ic_${method}.png`}
-                                  sx={{ width: 32, height: 32, position: 'absolute', right: 0 }}
-                              />
-                          </Tooltip>
-                      </Stack>
+                            <Tooltip title={method} placement="left">
+                                <Box
+                                    component="img"
+                                    alt={method}
+                                    src={`/assets/icons/auth/ic_${method}.png`}
+                                    sx={{ width: 32, height: 32, position: 'absolute', right: 0 }}
+                                />
+                            </Tooltip>
+                        </Stack>
 
-                      {/* <Alert severity="info" sx={{ mb: 3 }}> */}
-                      {/*   Use email : <strong>sistemas@hipertronics.us</strong> / password :<strong> ,2023;MongoDB</strong> */}
-                      {/* </Alert> */}
+                        {/* <Alert severity="info" sx={{ mb: 3 }}> */}
+                        {/*   Use email : <strong>sistemas@hipertronics.us</strong> / password :<strong> ,2023;MongoDB</strong> */}
+                        {/* </Alert> */}
 
-                      <AuthLoginForm />
+                        <AuthLoginForm />
 
-                      <AuthWithSocial />
+                        <AuthWithSocial />
 
-                  <Stack spacing={2} sx={{mb: 5, position: 'relative'}}>
-                      {/* <Label variant="filled" color="primary" startIcon={<Iconify icon="eva:info-fill" />}> */}
-                      {/*     {latitude && <p>Latitud: {latitude}</p>} */}
-                      {/* </Label> */}
-                      {/* <Label variant="filled" color="primary" startIcon={<Iconify icon="eva:info-fill" />}> */}
-                      {/*     {longitude && <p>Longitud: {longitude}</p>} */}
-                      {/* </Label> */}
+                        <Stack spacing={2} sx={{mb: 5, position: 'relative'}}>
+                            <Label variant="filled" color="primary" startIcon={<Iconify icon="eva:info-fill" />}>
+                                {latitude && <p>Latitud: {latitude}</p>}
+                            </Label>
+                            <Label variant="filled" color="primary" startIcon={<Iconify icon="eva:info-fill" />}>
+                                {longitude && <p>Longitud: {longitude}</p>}
+                            </Label>
+                        </Stack>
 
-                          {/* <CardHeader title="Change Theme" /> */}
-                          { latitude && longitude && (
-                          <CardContent>
-                              <StyledMapContainer>
-                                  <MapChangeTheme {...baseSettings} latitude={latitude} longitude={longitude} />
-                              </StyledMapContainer>
-                          </CardContent>
-
-                          )}
-
-
-                  </Stack>
-
-              </div>
-          )}
-      </div>
-      </LoginLayout>
-  );
+                    </div>
+                )}
+            </div>
+        </LoginLayout>
+    );
 }
-
-const StyledMapContainer = styled('div')(({ theme }) => ({
-    zIndex: 0,
-    height: 260,
-    overflow: 'hidden',
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    '& .mapboxgl-ctrl-logo, .mapboxgl-ctrl-bottom-right': {
-        display: 'none',
-    },
-}));
-
-const baseSettings = {
-    mapboxAccessToken: MAP_API,
-    minZoom: 1,
-};
-
-
-
