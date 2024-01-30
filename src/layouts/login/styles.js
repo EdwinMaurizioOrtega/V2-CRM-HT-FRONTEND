6f3e1e180a20437d7fccf38f16deaa12ef38c942
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2024-01-30 15:43:48 -0500

    Ajuste en los estilos Login v2

--- src/layouts/login/styles.js
+++ src/layouts/login/styles.js
@@ -26,7 +26,7 @@ export const StyledSection = styled('div')(({ theme }) => ({
 export const StyledSectionBg = styled('div')(({ theme }) => ({
   ...bgGradient({
     color: alpha(theme.palette.background.default, theme.palette.mode === 'light' ? 0.9 : 0.94),
-    imgUrl: '/assets/background/overlay_2.jpg',
+    imgUrl: '/assets/illustrations/login.jpg',
   }),
   top: 0,
   left: 0,
