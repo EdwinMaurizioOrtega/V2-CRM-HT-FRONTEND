636e84021a7361da4f86bd7384129907e7724b7e
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2024-02-22 12:31:39 -0500

    Buscar por nombre de usuario en el mapa.

--- src/pages/dashboard/user/tracking.js
+++ src/pages/dashboard/user/tracking.js
@@ -1,26 +1,17 @@
 // next
 import Head from 'next/head';
 // @mui
-import {alpha, styled} from '@mui/material/styles';
+import {styled} from '@mui/material/styles';
 import {
     Container,
-    Typography,
-    Box,
     Card,
-    TextField,
-    CardContent, Button, ListItem, ListItemText, List, Grid
+    CardContent, Grid, TextField
 } from '@mui/material';
 // layouts
 import DashboardLayout from '../../../layouts/dashboard';
 // components
 import {useSettingsContext} from '../../../components/settings';
 import React, {useEffect, useRef, useState} from "react";
-import {
-    GridToolbarColumnsButton,
-    GridToolbarContainer, GridToolbarDensitySelector, GridToolbarExport,
-    GridToolbarFilterButton,
-    GridToolbarQuickFilter
-} from "@mui/x-data-grid";
 import _mock from "../../../_mock";
 import CustomBreadcrumbs from "../../../components/custom-breadcrumbs";
 import {PATH_DASHBOARD} from "../../../routes/paths";
@@ -35,20 +26,10 @@ TrackingPage.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
 // ----------------------------------------------------------------------
 
-const THEMES = {
-    streets: 'mapbox://styles/mapbox/streets-v11',
-    outdoors: 'mapbox://styles/mapbox/outdoors-v11',
-    light: 'mapbox://styles/mapbox/light-v10',
-    dark: 'mapbox://styles/mapbox/dark-v10',
-    satellite: 'mapbox://styles/mapbox/satellite-v9',
-    satelliteStreets: 'mapbox://styles/mapbox/satellite-streets-v11',
-};
-
 const baseSettings = {
     mapboxAccessToken: MAP_API,
     minZoom: 1,
 };
-
 
 const StyledMapContainer = styled('div')(({theme}) => ({
     zIndex: 0,
@@ -66,78 +47,19 @@ export default function TrackingPage(callback, deps) {
 
     const {user} = useAuthContext();
 
-    const [messages, setMessages] = useState([]);
     const [coordinates, setCoordinates] = useState([])
-
-    const [input, setInput] = useState("");
-    const [currentRoom, setCurrentRoom] = useState("General");
     const [currentRoomMap, setCurrentRoomMap] = useState("Lidenar");
     const [name, setName] = useState(null);
     const [socket, setSocket] = useState(null);
     const [connected, setConnected] = useState(false);
     const onceRef = useRef(false);
-
     const [countriesData, setCountriesData] = useState([]);
-
-    // useEffect(() => {
-    //     setMessages([]);
-    //     socket?.emit("join", currentRoom);
-    // }, [currentRoom]);
-    //
-    // useEffect(() => {
-    //     if (onceRef.current) {
-    //         return;
-    //     }
-    //
-    //     onceRef.current = true;
-    //
-    //     const socket = io("ws://localhost:80");
-    //     //const socket = io("wss://ss.lidenar.com");
-    //     setSocket(socket);
-    //
-    //     // CHAT
-    //
-    //     socket.on("connect", () => {
-    //         console.log("Connected to socket server");
-    //         setName(`anon-${socket.id}`);
-    //         setConnected(true);
-    //         console.log("joining room", currentRoom);
-    //
-    //         socket.emit("join", currentRoom);
-    //     });
-    //
-    //     socket.on("message", (msg) => {
-    //         console.log("Message received AAA", msg);
-    //         msg.date = new Date(msg.date);
-    //         setMessages((messages) => [...messages, msg]);
-    //     });
-    //
-    //     socket.on("messages", (msgs) => {
-    //         console.log("Messages received BBB", msgs);
-    //         let messages = msgs.messages.map((msg) => {
-    //             msg.date = new Date(msg.date);
-    //             return msg;
-    //         });
-    //         setMessages(messages);
-    //
-    //     });
-    //
-    // }, []);
-    //
-    // const sendMessage = (e) => {
-    //     e.preventDefault();
-    //     socket?.emit("message", {
-    //         text: input,
-    //         room: currentRoom,
-    //     });
-    //     setInput("");
-    // };
+    const [filterUserName, setFilterUserName] = useState('');
 
     useEffect(() => {
-        setMessages([]);
+        setCoordinates([]);
         socket?.emit("get_coordinates", currentRoomMap)
     }, [currentRoomMap]);
-
 
     useEffect(() => {
         if (onceRef.current) {
@@ -203,13 +125,12 @@ export default function TrackingPage(callback, deps) {
 
         });
 
-
     }, []);
 
     useEffect(() => {
         if (coordinates.length > 0) {
 
-            console.log("messages: "+ JSON.stringify(coordinates));
+            console.log("messages: " + JSON.stringify(coordinates));
             const objectArray = [];
 
             // Definir los valores estáticos para todos los países
@@ -224,16 +145,18 @@ export default function TrackingPage(callback, deps) {
             // Iterar sobre todos los mensajes y obtener las coordenadas de cada uno
             coordinates.forEach((coor) => {
                 // Convertir el texto del mensaje a JSON y obtener las coordenadas
-                const { latitud, longitud, date, user_name } = coor
+                const {latitud, longitud, date, user_name} = coor
 
                 // Agregar las coordenadas al objeto estático y agregarlo al array
-                const country = {
-                    ...staticValues,
-                    latlng: [latitud, longitud],
-                    capital: date.toLocaleString(), //Fecha/Hora
-                    name: user_name //Usuario
-                };
-                objectArray.push(country);
+                if (filterUserName === '' || user_name === filterUserName) {
+                    const country = {
+                        ...staticValues,
+                        latlng: [latitud, longitud],
+                        capital: date.toLocaleString(),
+                        name: user_name
+                    };
+                    objectArray.push(country);
+                }
             });
 
             console.log("Lista de todos los países:", objectArray);
@@ -241,8 +164,11 @@ export default function TrackingPage(callback, deps) {
             // Establecer los países como datos
             setCountriesData(objectArray);
         }
-    }, [coordinates]);
+    }, [coordinates, filterUserName]);
 
+    const handleFilterChange = (event) => {
+        setFilterUserName(event.target.value);
+    };
 
     return (
         <>
@@ -268,97 +194,29 @@ export default function TrackingPage(callback, deps) {
                     ]}
                 />
 
-
                 <div className="flex">
                     <Grid container spacing={2}>
                         <Grid item xs={12} md={12}>
                             <Card>
                                 <CardContent>
+                                    <TextField
+                                        label="Filtrar por nombre de usuario"
+                                        variant="outlined"
+                                        value={filterUserName}
+                                        onChange={handleFilterChange}
+                                        fullWidth
+                                        margin="normal"
+                                    />
                                     <StyledMapContainer>
                                         <MapMarkersPopups {...baseSettings} data={countriesData}/>
                                     </StyledMapContainer>
                                 </CardContent>
                             </Card>
                         </Grid>
-                        {/*<Grid item xs={12} md={6}>*/}
-                        {/*    <Card>*/}
-                        {/*        <div className="h-screen p-4 bg-ctp-crust flex flex-col flex-grow justify-end">*/}
-                        {/*            <div style={{maxHeight: '300px', overflowY: 'auto'}}>*/}
-                        {/*                <List>*/}
-                        {/*                    {messages?.map((msg, index) => (*/}
-                        {/*                        <ListItem key={index} alignItems="flex-start">*/}
-                        {/*                            <ListItemText*/}
-                        {/*                                // primary={msg.user}*/}
-                        {/*                                //primary={JSON.parse(msg.text).user}*/}
-                        {/*                                primary={msg.user}*/}
-                        {/*                                secondary={*/}
-                        {/*                                    <>*/}
-                        {/*                                        <Typography*/}
-                        {/*                                            component="span"*/}
-                        {/*                                            variant="body2"*/}
-                        {/*                                            color="textPrimary"*/}
-                        {/*                                        >*/}
-                        {/*                                            {msg.date.toLocaleString()}*/}
-                        {/*                                        </Typography>*/}
-                        {/*                                        <br/> /!* Agrega un salto de línea *!/*/}
-                        {/*                                        <Typography*/}
-                        {/*                                            component="span"*/}
-                        {/*                                            variant="body1"*/}
-                        {/*                                            color="textPrimary"*/}
-                        {/*                                        >*/}
-                        {/*                                            {msg.text}*/}
-                        {/*                                        </Typography>*/}
-                        {/*                                    </>*/}
-                        {/*                                }*/}
-                        {/*                            />*/}
-                        {/*                        </ListItem>*/}
-                        {/*                    ))}*/}
-                        {/*                </List>*/}
-                        {/*            </div>*/}
-                        {/*            <form className="flex h-11" onSubmit={sendMessage}>*/}
-                        {/*                <TextField*/}
-                        {/*                    type="text"*/}
-                        {/*                    value={input}*/}
-                        {/*                    onChange={(e) => setInput(e.target.value)}*/}
-                        {/*                    variant="filled"*/}
-                        {/*                    fullWidth*/}
-                        {/*                    placeholder="Escribe un mensaje..."*/}
-                        {/*                />*/}
-                        {/*                <Button*/}
-                        {/*                    type="submit"*/}
-                        {/*                    variant="contained"*/}
-                        {/*                    color="primary"*/}
-                        {/*                    className="ml-2"*/}
-                        {/*                >*/}
-                        {/*                    Enviar*/}
-                        {/*                </Button>*/}
-                        {/*            </form>*/}
-                        {/*        </div>*/}
-                        {/*    </Card>*/}
-                        {/*</Grid>*/}
                     </Grid>
                 </div>
-
             </Container>
 
         </>
     );
 }
-
-// ----------------------------------------------------------------------
-
-function CustomToolbar() {
-    return (
-        <GridToolbarContainer>
-            <GridToolbarQuickFilter/>
-            <Box sx={{flexGrow: 1}}/>
-            <GridToolbarColumnsButton/>
-            <GridToolbarFilterButton/>
-            <GridToolbarDensitySelector/>
-            <GridToolbarExport/>
-        </GridToolbarContainer>
-    );
-}
-
-// ----------------------------------------------------------------------
-
