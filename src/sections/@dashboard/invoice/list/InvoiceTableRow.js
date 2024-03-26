93676df02c08db435ddbb2baf6f5159bde8826d1
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2024-03-26 11:21:09 -0500

    Anular Ordenes de Venta en el SAP desde el CRM.

--- src/sections/@dashboard/invoice/list/InvoiceTableRow.js
+++ src/sections/@dashboard/invoice/list/InvoiceTableRow.js
@@ -227,7 +227,8 @@ export default function InvoiceTableRow({
         try {
             const response = await axios.put('/hanadb/api/orders/order/to_bag', {
                 params: {
-                    ID_ORDER: ID
+                    ID_ORDER: ID,
+                    DOCNUM: Number(DOCNUM)
                 }
             });
 
@@ -520,7 +521,7 @@ export default function InvoiceTableRow({
 
                 <Divider sx={{borderStyle: 'dashed'}}/>
 
-                {user.ROLE === "aprobador" ? (
+                {ESTADO === 6 && user.ROLE === "aprobador" ? (
                     <MenuItem
                         onClick={() => {
                             handleOpenConfirmAnular();
