01cd3f9727edc0199ea7df64fdd216509258fa4a
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2024-07-02 22:09:27 -0500

    Ajuste URLs

--- next.config.js
+++ next.config.js
@@ -4,12 +4,12 @@ module.exports = {
   env: {
 
     // HOST LOCAL PRIVATE
-    // HOST_API_KEY: 'http://192.168.88.120:7070',
-    // HOST_SOCKET: 'ws://192.168.88.120:80',
+    HOST_API_KEY: 'http://192.168.88.120:7070',
+    HOST_SOCKET: 'ws://192.168.88.120:80',
 
     // HOST LOCAL
-    HOST_API_KEY: 'http://localhost:7070',
-    HOST_SOCKET: 'ws://localhost:80',
+    // HOST_API_KEY: 'http://localhost:7070',
+    // HOST_SOCKET: 'ws://localhost:80',
 
     // HOST REMOTO
     // HOST_API_KEY: 'https://crm.lidenar.com',
