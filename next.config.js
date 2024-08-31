71d1e8c8e264b97d973d1a52bb6c9af004ca0e77
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2024-08-30 19:08:41 -0500

    Nuevo Login CRM HT v1.1

--- next.config.js
+++ next.config.js
@@ -8,12 +8,12 @@ module.exports = {
     // HOST_SOCKET: 'ws://192.168.88.120:80',
 
     // HOST LOCAL
-    // HOST_API_KEY: 'http://localhost:7070',
-    // HOST_SOCKET: 'ws://localhost:80',
+    HOST_API_KEY: 'http://localhost:7070',
+    HOST_SOCKET: 'ws://localhost:80',
 
     // HOST REMOTO
-    HOST_API_KEY: 'https://crm.lidenar.com',
-    HOST_SOCKET: 'wss://ss.lidenar.com',
+    // HOST_API_KEY: 'https://crm.lidenar.com',
+    // HOST_SOCKET: 'wss://ss.lidenar.com',
 
     // MAPBOX
     MAPBOX_API: 'pk.eyJ1Ijoic2lzdGVtYXMtMjEwMCIsImEiOiJjbHJwM2p6OTgwMG1vMnFvNjZ5dnM5OHo4In0.WfTKE_C5op0Ameu3Llhldw',
