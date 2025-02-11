0b0dbe66939407e2a026270e26d29a8d83c7729c
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2025-02-11 10:42:59 -0500

    Power BI MC Gerencia.

--- src/pages/dashboard/microsoft-power-bi/gerencia-power-bi.js
+++ src/pages/dashboard/microsoft-power-bi/gerencia-power-bi.js
@@ -1,26 +1,14 @@
 // next
 import Head from 'next/head';
 // @mui
-import { useTheme } from '@mui/material/styles';
-import {Grid, Container, Typography, Box} from '@mui/material';
+import {useTheme} from '@mui/material/styles';
+import {Container, Box} from '@mui/material';
 // layouts
 import DashboardLayout from '../../../layouts/dashboard';
-// _mock_
-//import { _analyticPost, _analyticOrderTimeline, _analyticTraffic } from '../../_mock/arrays';
 // components
-import { useSettingsContext } from '../../../components/settings';
-// sections
-// import {
-//   AnalyticsTasks,
-//   AnalyticsNewsUpdate,
-//   AnalyticsOrderTimeline,
-//   AnalyticsCurrentVisits,
-//   AnalyticsWebsiteVisits,
-//   AnalyticsTrafficBySite,
-//   AnalyticsWidgetSummary,
-//   AnalyticsCurrentSubject,
-//   AnalyticsConversionRates,
-// } from '../../../sections/@dashboard/general/analytics';
+import {useSettingsContext} from '../../../components/settings';
+import {useAuthContext} from "../../../auth/useAuthContext";
+import {useEffect, useState} from "react";
 
 // ----------------------------------------------------------------------
 
@@ -29,43 +17,48 @@ GeneralAnalyticsPage.getLayout = (page) => <DashboardLayout>{page}</DashboardLay
 // ----------------------------------------------------------------------
 
 export default function GeneralAnalyticsPage() {
-  const theme = useTheme();
 
-  const { themeStretch } = useSettingsContext();
+    const {user} = useAuthContext();
 
-  return (
-    <>
-      <Head>
-        <title> Gerencia: Power BI</title>
-      </Head>
+    const [urlEmpresa, setUrlEmpresa] = useState('');
 
-      <Container maxWidth={themeStretch ? false : 'xl'}>
+    useEffect(() => {
 
-        {/*<Typography variant="h4" sx={{ mb: 5 }}>*/}
-        {/*  Gerencia*/}
-        {/*</Typography>*/}
+        //Lidenar
+        if (user.EMPRESA === '0992537442001') {
+            setUrlEmpresa('https://app.powerbi.com/view?r=eyJrIjoiYzdjZTliNTMtYjViMi00YzczLWFkMDgtYjQ0ZTE2ZTNiMTJjIiwidCI6Ijk2ZTEzNWRiLWZmZDItNDdiZS1iZDJlLWYxMjQ2MTY2YWZmZCIsImMiOjR9')
+        } else {
+            // MovilCelistic
+            setUrlEmpresa('https://app.powerbi.com/view?r=eyJrIjoiMTgwODFlMjUtMzdkOC00YTU4LTlhZTMtYTdkYTMwODk2NWM5IiwidCI6Ijk2ZTEzNWRiLWZmZDItNDdiZS1iZDJlLWYxMjQ2MTY2YWZmZCIsImMiOjR9')
+        }
 
-        <Box
-            component="iframe"
-            src="https://app.powerbi.com/view?r=eyJrIjoiYzdjZTliNTMtYjViMi00YzczLWFkMDgtYjQ0ZTE2ZTNiMTJjIiwidCI6Ijk2ZTEzNWRiLWZmZDItNDdiZS1iZDJlLWYxMjQ2MTY2YWZmZCIsImMiOjR9"
-            sx={{
-              width: '100%',
-              height: '1000px',
-              border: 'none',
-            }}
-        />
+    }, [user]);
 
+    const {themeStretch} = useSettingsContext();
 
+    return (
+        <>
+            <Head>
+                <title> Gerencia: Power BI</title>
+            </Head>
 
+            <Container maxWidth={themeStretch ? false : 'xl'}>
 
+                {/*<Typography variant="h4" sx={{ mb: 5 }}>*/}
+                {/*  Gerencia*/}
+                {/*</Typography>*/}
 
+                <Box
+                    component="iframe"
+                    src={urlEmpresa}
+                    sx={{
+                        width: '100%',
+                        height: '1000px',
+                        border: 'none',
+                    }}
+                />
 
-
-
-
-
-
-      </Container>
-    </>
-  );
+            </Container>
+        </>
+    );
 }
