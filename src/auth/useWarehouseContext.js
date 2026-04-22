da066d2d935dff0faefaf4208d4cd5f3ba82548a
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2026-04-22 11:46:31 -0500

    ajuste v0.2

--- src/auth/useWarehouseContext.js
+++ src/auth/useWarehouseContext.js
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { WarehouseContext } from './WarehouseContext';
 
 export const useWarehouseContext = () => {
-    const context = useContext(WarehouseContext);
+    const context = useContext(WarehouseContext) ;
 
     if (!context) throw new Error('useWarehouseContext must be used inside WarehouseProvider');
 
